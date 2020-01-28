a8ff9df0e38280a25db57381797fd8fa13a25f0c
yairb <yairb@jfrog.com>
2020-01-28 16:27:46 +0200

    Seperated both services to different images

---
+++ tutorial/step1-create-gradle-app/wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar /ws/frogsws.jar
+echo frogsws
---
+++ tutorial/step2-create-ui-pkg/wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nginx
+echo nginx
+#java -jar /ws/frogsws.jar
+echo frogsws
+npm start --prefix /usr/share/nginx/html/frogsui
+echo frogsui
